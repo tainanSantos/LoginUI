5a52a70db8719847c6ccc541f7d5c0e1eae245d4
Tainan Santos <tainan206@gmail.com>
2021-04-25 17:40:20 -0300

    Pagina inicial refeita

--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -15,42 +15,90 @@ class _LoginPageState extends ModularState<LoginPage, LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            end: Alignment.topRight,
-            begin: Alignment.bottomLeft,
-            colors: [
-              Colors.deepPurple,
-              Colors.blue,
-            ], // red to yellow
-            tileMode: TileMode.repeated, // repeats the gradient over the canvas
-          ),
-        ),
-        child: Container(
-          padding: EdgeInsets.all(30),
-          child: Center(
-            child: Wrap(
-              children: [
-                Container(
-                  height: 45,
-                  width: MediaQuery.of(context).size.width,
-                  child: RaisedButton(
-                    color: Colors.white,
-                    onPressed: () {},
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        color: Colors.deepPurple,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                end: Alignment.topRight,
+                begin: Alignment.bottomLeft,
+                colors: [
+                  Colors.deepPurple,
+                  Colors.blue,
+                ], // red to yellow
+                tileMode:
+                    TileMode.repeated, // repeats the gradient over the canvas
+              ),
             ),
           ),
-        ),
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: Image.asset(
+              'assets/images/login.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            color: Colors.black38,
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height / 9,
+            left: 10,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Helo!\nWelcome",
+                style: TextStyle(color: Colors.white, fontSize: 26),
+              ),
+            ),
+          ),
+          Positioned(
+              left: 10,
+              right: 10,
+              bottom: 10,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      margin: EdgeInsets.all(8.0),
+                      height: 50,
+                      width: 120,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white),
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                          child: Text(
+                        "Sign up",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      )),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      margin: EdgeInsets.all(8.0),
+                      height: 50,
+                      width: 120,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                          child: Text(
+                        "Log in",
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      )),
+                    ),
+                  ),
+                ],
+              ))
+        ],
       ),
     );
   }
